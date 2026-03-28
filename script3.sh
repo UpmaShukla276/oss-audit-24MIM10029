0c88a38d7b81be77353329545eb43833be080ef5
Upma Shukla <upma.shukla38@gmail.com>
2026-03-28 16:27:22 +0000

    Script 3 completed

---
+++ script3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Script 3: Disk and Permission Auditor
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report"
+echo "----------------------"
+
+for DIR in "${DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist on this system"
+    fi
+done
+
+echo "----------------------"
+
+# Added TODO part (for Git config directory)
+CONFIG_DIR="$HOME/.git"
+
+if [ -d "$CONFIG_DIR" ]; then
+    PERMS=$(ls -ld $CONFIG_DIR | awk '{print $1, $3, $4}')
+    echo "Git Config Dir ($CONFIG_DIR) => Permissions: $PERMS"
+else
+    echo "Git config directory not found"
+fi
